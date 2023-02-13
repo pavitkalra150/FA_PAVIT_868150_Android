8d6b6b775a4a1c6199d153f5fa3b7af940c5887c
pavitkalra150 <c0868150@mylambton.ca>
2023-02-13 18:31:50 -0500

    Created Database Helper class

---
+++ app/src/main/java/com/example/fa_pavit_868150_android/DatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.fa_pavit_868150_android;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    // Database information
+    private static final String DATABASE_NAME = "products.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Table information
+    private static final String TABLE_PRODUCTS = "products";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_PRICE = "price";
+    private static final String COLUMN_LATITUDE = "latitude";
+    private static final String COLUMN_LONGITUDE = "longitude";
+
+    private static final String CREATE_TABLE_PRODUCTS = "CREATE TABLE " + TABLE_PRODUCTS + "("
+            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_NAME + " TEXT,"
+            + COLUMN_DESCRIPTION + " TEXT,"
+            + COLUMN_PRICE + " REAL,"
+            + COLUMN_LATITUDE + " REAL,"
+            + COLUMN_LONGITUDE + " REAL" + ")";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_PRODUCTS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/fa_pavit_868150_android/ProductDAO.java
@@ -0,0 +1,4 @@
+package com.example.fa_pavit_868150_android;
+
+public class ProductDAO {
+}
